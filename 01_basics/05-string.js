ba5b6831107fe12fc17405863d6ed25038edcc36
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:16:06 +0530

    strings

---
+++ 01_basics/05-string.js
@@ -0,0 +1,32 @@
+// let name = "Govardhan"
+// let age = 22
+//console.log(name + age);  // not recomended
+//console.log(`iam ${name} and my age is ${age}`);  //recomended
+
+// other way to declare a string
+// let gameName = new String("franky")
+//console.log(typeof gameName);   // object
+//console.log(gameName[4]);
+//console.log(gameName.__proto__);  // {} empty object
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// let newString = "superFranky"
+// console.log(newString.substring(0, 4));
+// console.log(newString.slice(-4, -6));
+
+// let anotherString = "   Govardhan   "
+// console.log(anotherString);
+// console.log(anotherString.trim());
+
+// let url = "https://govardhan.com/super%20franky"
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes("franky"));
+// console.log(url.includes("cutty-flam"));
+
+
+// let fName = "Kyasani-Govardhan-Super_Franky"
+// let arrayOfString = fName.split("-")
+// console.log(arrayOfString);
