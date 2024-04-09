c104e55852ec8f8d30d6dec4550727bc60ff7c78
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:15:05 +0530

    conversions

---
+++ 01_basics/03-conversions.js
@@ -0,0 +1,106 @@
+
+    //To Number
+// let salary = false
+// console.log(typeof salary);
+// let valueInNumber = Number(salary)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  
+
+/*
+ other type to Number
+  "33" => 33
+  "33RC" => NaN
+  null => 0
+  undefined => NaN 
+  true => 1 , false => 0
+*/
+
+
+   //TO Boolean
+// let isElligible = null
+// console.log(typeof isElligible);
+// let toBoolean = Boolean(isElligible)
+// console.log(typeof toBoolean);
+// console.log(toBoolean);
+  
+
+/* 
+   other type to boolean
+   1 and other than 0 => true
+   0 => false
+   "" => false
+   "govardhan" => true
+   undefined => false
+
+*/
+
+
+       //TO String
+   //  let someNumber = NaN
+   //  console.log(typeof someNumber);
+   //  let toString = String(someNumber)
+   //  console.log(typeof toString);
+   //  console.log(toString);    
+
+
+
+   //***********************    Operations     ************************************
+
+   // let value = 30
+   // let negativeValue = -value
+   // console.log(negativeValue);
+
+   // console.log(2 + 2);
+   // console.log(2 - 2);
+   // console.log(2 * 2);
+   // console.log(2 ** 2);
+   // console.log(2 / 2);
+   // console.log(2 % 2);
+ 
+   // let greeting = "hello!"
+   // let name = " Govardhan"
+   // let message = greeting+name
+   // console.log(message);
+
+   // console.log(1 + 2);
+   // console.log(1 + "2");
+   // console.log("1" + 2);
+   // console.log("1" + 2 + 2);
+   // console.log(1 + 2 + "2");
+
+   // console.log(true);    
+   // console.log(+true);  not recomemded
+   // console.log(+"");   not recomemded
+
+   // let base = "23"
+   // let hike = "4"
+   // console.log((+base) + (+hike));   not recomended
+
+   // let counter = 0
+   //  counter + +
+   // // counter + 5
+   // // counter = counter + 5
+   // console.log(counter);
+
+   
+ 
+   //postfix operation
+
+   /* If used postfix, with operator after operand (for example, x++), 
+   the increment operator increments and returns the value before incrementing.
+   */
+   // let x = 3
+   // const y = x++
+   // console.log(`x:${x},y:${y}`);
+
+   //prefix operation
+   
+   /*If used prefix, with operator before operand (for example, ++x),
+    the increment operator increments and returns the value after incrementing.
+   */
+   // let a = 3
+   // const b = ++a
+   // console.log(`a:${a},b:${b}`);
+
+
+
