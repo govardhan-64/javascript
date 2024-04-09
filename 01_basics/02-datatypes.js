98cfcf1446b071a370415a04aeece359fe09573a
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:14:51 +0530

    data types

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,25 @@
+//"use strict";  -> treat all js code as newer version
+
+//alert('hello')  -> works in browser not in node js
+
+let name = "Govardhan"
+let age = 23
+let isElligible = false
+let temperature = null
+let state;
+let component = Symbol
+
+console.log(typeof age);
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => value of variable not defined
+//symbol => to give uniqueness
+
+
+//console.log(typeof undefined);  //undefined
+//console.log(typeof null);       //object
+console.log(typeof component);    //function
