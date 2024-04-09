ec8f66bed2c3c68542aa3dba6dc0e53f3bf60f0c
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:17:56 +0530

    objects 2

---
+++ 02_basics/04-objects.js
@@ -0,0 +1,14 @@
+
+const instaUser = new Object()   // singleton object
+console.log(instaUser);
+
+// const xUser = {}                // non-singleton object
+// console.log(xUser);
+
+instaUser.id = "854432"
+instaUser.name = "franky_super"
+instaUser.isLoggedIn = false
+console.log(instaUser);
+
+
+
