0835ff4d43b0e8120a3d971b134d412a1522090c
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:15:28 +0530

    comprisions

---
+++ 01_basics/04-comparisions.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);  //true 
+// console.log("02" > 1);   //true
+
+// console.log(null > 0);    //false
+// console.log(null >= 0);   //true  because , it is converted to number and treated as 0 
+// console.log(null < 0);    //false
+// console.log(null <= 0);   //true  because , it is converted to number and treated as 0
+// console.log(null == 0);   //false   
+// console.log(null === 0);   //false
+
+// console.log(undefined > 0);  //false
+// console.log(undefined >= 0);  //false
+// console.log(undefined < 0);   //false
+// console.log(undefined <= 0);  //false
+// console.log(undefined == 0);   //false
+// console.log(undefined === 0);   //false
+
+
+// console.log(null <= undefined);  //false
+// console.log(null == undefined);  //true
+
+
+// console.log("2" == 2);   //true
+// console.log("2" === 2);   //false
+ 
