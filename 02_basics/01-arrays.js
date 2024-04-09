592025883477c027315d6c86d650ed18cfb24c20
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:17:16 +0530

    arrays 1

---
+++ 02_basics/01-arrays.js
@@ -0,0 +1,71 @@
+// arrays 
+
+ /*The Array object, as with arrays in other programming languages,
+  enables storing a collection of multiple items under a single variable name */
+
+const myArr = [0,1,2,3,4,5]
+// console.log(myArr);
+
+// console.log(myArr["4"]); // wrong  way
+// console.log(myArr[4]);      // correct  way
+// console.log(myArr[6]);
+
+// const heros = ["bat","spyder","aqua","super"]
+// const myArr2 = new Array(2,4,6,8,)
+// console.log(myArr2[1]);
+
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(8)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(4));   //true
+// console.log(myArr.includes(9));   //false
+
+// console.log(myArr.indexOf(4));    // 4
+// console.log(myArr.indexOf(9));    // -1
+// console.log(myArr.indexOf(12));   // -1
+
+
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+// slice, splice
+
+// console.log("A", myArr);
+// const myn1 = myArr.slice(1,3)
+// console.log("slice",myn1);
+// console.log("B", myArr);
+
+//       console.log("\n");
+
+// console.log("C", myArr);
+// const myn2 = myArr.splice(1,3)
+// console.log("splice",myn2);
+// console.log("D", myArr);
+
+
+
+
+
+
+
+
+ 
