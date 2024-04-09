09cd37c1041217112888d17b9e131213a920bc3c
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:14:26 +0530

    basics

---
+++ 01_basics/01-basics.js
@@ -0,0 +1,25 @@
+const userId = 'E4322'
+let userEmail = "srk@google.com"
+var salary = 55600
+userCity = "mumbai"
+
+
+
+//userId = "R4355";   ->not allowed
+//const name;   // -> not allowed because, const declarations must be defined 
+
+console.log(userId);
+userEmail = "sk@google.com"
+salary = 44000
+userCity = "pune"
+let age;
+
+console.table([userEmail,salary,userCity, age]);
+
+/*
+   using var is not recommended 
+   because it does not have any scope.
+   so, use only const , let to declare varisbles
+*/
+
+
