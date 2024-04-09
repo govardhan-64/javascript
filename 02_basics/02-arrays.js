03f79924f9fcebcd820bf74b46c318e0daf17385
govardhan64 <kyasanigovardhan776@gmail.com>
2024-04-09 11:17:27 +0530

    arrays 2

---
+++ 02_basics/02-arrays.js
@@ -0,0 +1,104 @@
+const strawHat = ["luffy","zoro","sanji"]
+const heart = ["law","beppo"]
+const kid = ["kid","killer"]
+
+// strawHat.push(heart)         //not recomended
+// console.log(strawHat);
+// console.log(strawHat[2]);     
+// console.log(strawHat[3]);
+// console.log(strawHat[3][1]);
+
+        // strawHat.concat(heart)    // wrong
+        // console.log(strawHat); 
+        
+        
+    // let alliance = strawHat.concat(heart)   //correct
+    // console.log(alliance);  
+
+// console.log(strawHat.concat(heart));
+
+    // let newAlliance = strawHat.concat(heart).concat(kid)  
+    // console.log(newAlliance);
+     
+// console.log(strawHat.concat(heart).concat(kid));
+
+
+
+    // let alliance =  [...strawHat,...heart]
+    // console.log(alliance);
+
+//  console.log([...strawHat,...heart]);
+
+    // let newAlliance = [...strawHat,...heart,...kid]
+    // console.log(newAlliance);
+
+// console.log([...strawHat,...heart,...kid]);    
+
+
+
+
+//  const onionArray = [1,2,3,[4,5],[6,7,[8,9]]] 
+
+//  let plainArray0 = onionArray.flat(0)
+//  console.log(plainArray0);
+
+//  let plainArray1 = onionArray.flat(1)
+//  console.log(plainArray1);
+
+//  let plainArray2 = onionArray.flat(2)
+//  console.log(plainArray2);
+
+//  let plainArray = onionArray.flat(Infinity)
+//  console.log(plainArray);
+
+
+
+
+//  Used  for data scraping
+
+// let treasure = "OnePiece"
+// console.log(treasure);
+// console.log(Array.isArray(treasure));   // to check array or not
+
+// let treasureInArray = Array.from(treasure)    // to change to array
+// console.log(treasureInArray);
+// console.log(treasure);               // since, only copy is changed
+
+
+
+// let appearance = {
+//     height: 5.8,
+//     color: "brown",
+//     hairColor: "black",
+//     physic: "skinny"
+// }
+
+// let appearanceInArray = Array.from(appearance)
+// console.log(appearanceInArray);         // empty array because, not specified - key or value 
+
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// let score4 = 400
+// let score5 = 500
+
+// let scoresInArray = Array.of(score1,score2,score3,score4,score5)
+// console.log(scoresInArray);
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
