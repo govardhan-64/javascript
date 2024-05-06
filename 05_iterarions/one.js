0f4358b0ec4a1bc7846b7c16f5849990bde895b0
govardhan64 <kyasanigovardhan776@gmail.com>
2024-05-06 20:27:21 +0530

    loops

---
+++ 05_iterarions/one.js
@@ -0,0 +1,57 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//    console.log(element);
+    
+// }
+
+// for (let k = 0; k < 10; k++) {
+//     const element = k;
+//     if(element == 6){
+//         console.log("6 is my favourite");
+//     }
+//     console.log(element);
+// }
+
+ 
+
+// tables
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i} table`);
+//     console.log("\t");
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     console.log("\n");
+// }
+
+
+
+// loop on arrays 
+// let myHeros = ["sper", "bat", "spyder", "aqua", "iron"]
+// for (let index = 0; index < myHeros.length; index++) {
+//     const element = myHeros[index];
+//     console.log(element);
+//     // console.log(myHeros.length);
+// }
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index === 5){
+//         console.log("detected value 5");
+//         break;
+//     }
+//     console.log(index);
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if(index === 5 ){
+        console.log(`detected value ${index}`);
+        continue;
+    }
+    console.log(index);
+}
---
+++ 05_iterarions/two.js
@@ -0,0 +1,22 @@
+// let index = 1
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index += 2
+// }
+
+
+// let cars = ['porshe','lambo','bmw','vw','tata']
+// let i = 0
+// while (i < cars.length) {
+//     console.log(cars[i]);
+//     i ++
+// }
+
+
+
+// // let score = 1
+// let score = 11
+// do {
+//     console.log(`score is ${score}`);  
+//     score ++  
+// } while (score <= 10);
